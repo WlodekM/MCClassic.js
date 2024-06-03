cb966bda9e45ad5d3140826c4c9776a15115daf3
WlodekM <wlodzio.maly@gmail.com>
2024-06-03 11:36:35 +0300

    fix plugins (just one lol)

--- plugins/cosmetics.js
+++ plugins/cosmetics.js
@@ -1,5 +1,5 @@
-export let server = (server, settings) => {
-    var methods = {
+export const server = (server, settings) => {
+    const methods = {
         AddCommand: function(player, server) {
             player.commands.add({
                 base: 'nick',
@@ -7,18 +7,39 @@ export let server = (server, settings) => {
                 usage: '/nick <nickname>',
                 op: false,
                 action(params) {
-                    let message
-                    if (params.toString().split(' ')[0] !== null) message = params.toString()
+                    let message;
+                    if (params.toString().split(' ')[0] !== null) message = params.toString();
 
                     server._writeAll('message', {
                         player_id: player.id,
                         message: `* ${player.username} ${message.split('%').join('&')}`
-                    })
+                    });
                 }
-            })
+            });
         }
+    };
+
+    // Directly use the methods if needed in the server function
+    // Alternatively, you can keep the methods declaration and export it below
+};
+
+// Export methods as named export
+export const methods = {
+    AddCommand: function(player, server) {
+        player.commands.add({
+            base: 'nick',
+            info: 'Allows you to change your name in chat.',
+            usage: '/nick <nickname>',
+            op: false,
+            action(params) {
+                let message;
+                if (params.toString().split(' ')[0] !== null) message = params.toString();
+
+                server._writeAll('message', {
+                    player_id: player.id,
+                    message: `* ${player.username} ${message.split('%').join('&')}`
+                });
+            }
+        });
     }
-
-    module.exports = methods;
-
-}
+};
